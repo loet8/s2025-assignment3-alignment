d0b4420182a6976b7ba6994dcce450bb0c8400dc
loet8 <156853311+loet8@users.noreply.github.com>
2025-04-23 17:27:44 -1000

    Update train.py

--- cs336_alignment/train.py
+++ cs336_alignment/train.py
@@ -1,7 +1,7 @@
-import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 from __future__ import annotations
 import argparse
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
 import json
 import logging
 import os
