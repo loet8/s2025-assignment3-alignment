38148f254d0ee6a1bddc47a2f1b2e31d95ed2b98
loet8 <156853311+loet8@users.noreply.github.com>
2025-04-24 18:27:34 -1000

    Update sft_evaluate_gsm8k.py

--- cs336_alignment/sft_evaluate_gsm8k.py
+++ cs336_alignment/sft_evaluate_gsm8k.py
@@ -1,4 +1,9 @@
 import os
+os.environ['VLLM_USE_V1'] = '0'
+
+import multiprocessing as mp
+mp.set_start_method("spawn", force=True)
+
 import sys
 import json
 import time
@@ -12,7 +17,7 @@ from tqdm import tqdm
 from typing import Any, List, Dict
 from tests import adapters  
 
-model_dir = "/content/models/qwen2.5-0.5B-sft"
+model_dir = "/content/sft_model_local"  
 gsm8k_path = "/content/data/gsm8k/dev.jsonl"
 batch_size = 5
 max_tokens = 512
@@ -47,7 +52,7 @@ def main():
     prompts = [format_instruction_prompt(ex) for ex in examples]
     labels = [ex["answer"].strip() for ex in examples]
 
-    model = LLM(model=model_dir)
+    llm = LLM(model=model_dir, gpu_memory_utilization=0.7, max_num_seqs=2)
     sampling_params = SamplingParams(temperature=0.0, top_p=1.0, max_tokens=max_tokens, stop=["\n"])
 
     print(f"Evaluating {len(prompts)} GSM8K examples...")
